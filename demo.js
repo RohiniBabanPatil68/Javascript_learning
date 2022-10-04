9526da92e29374c7a94d69880f1d796705470692
RohiniBabanPatil68 <beldarrohini8086@gmail.com>
2022-10-04 12:54:13 +0530

    first program execution in JS
    first program execution in JS

---
+++ demo.js
@@ -0,0 +1,8 @@
+
+console.log("Hello");
+console.log("My name is Rohini Patil");
+ console.info("My hobbies are Travelling, Listening Music & Drawing"); // info means information
+ console.log("My strenghts are Honesty,flexibility & learning new things")
+console.warn("My weaknesses are Nervousness about public speeking, impatience & too much thinking"); // warn means warning
+ console.log(30+50);
+ console.log(49*479);
